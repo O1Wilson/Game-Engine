8aacc449d6d6da4f1de767dc58b8e232f3c246af
Owen Wilson <Badgermouse583@gmail.com>
2026-08-12 21:26:57 -0500

    Setup basis of Engine
    Moved the main.cpp contents out now that the engine is ready to be constructed.
    
    Created new CMAKE file to setup engine directory.
    
    Setup architecture for the engine by making two classes: Engine and Application.
    
    Created basic methods in both classes

---
+++ engine/source/Application.cpp
@@ -0,0 +1,11 @@
+#include "Application.h"
+
+namespace eng {
+	void Application::SetNeedsToBeClosed(bool value) {
+		m_needsToBeClosed = value;
+	}
+
+	bool Application::NeedsToBeClosed() const {
+		return m_needsToBeClosed;
+	}
+}
