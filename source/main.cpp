2e5c80f384cbc95624f127431aa3790f5ea527cc
Owen Wilson <Badgermouse583@gmail.com>
2026-08-12 21:43:56 -0500

    Created Game Class and integrated the engine with the application

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,16 @@
+#include "Game.h"
+#include <eng.h>
+
 int main() {
+	Game* game = new Game();
+	eng::Engine engine;
+	engine.SetApplication(game);
+
+	if (engine.Init()) {
+		engine.Run();
+	}
+
+	engine.Destroy();
+
 	return 0;
 }
